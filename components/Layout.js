c598dd899beb71847a32e4164a22f9e1b1921c91
Blake Ingenthron <blakeingenthron@gmail.com>
2019-06-28 17:23:09 -0500

    intial commit

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+const Layout = props => {
+	return (
+		<div>
+			<Head>
+				<title>BitzPrice</title>
+				<link
+					rel="stylesheet"
+					href="https://bootswatch.com/4/cerulean/bootstrap.min.css"
+				/>
+			</Head>
+			<Navbar />
+			<div className="container">{props.children}</div>
+		</div>
+	);
+};
+
+export default Layout;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const about = () => {
+	return (
+		<Layout>
+			<div>hi</div>
+		</Layout>
+	);
+};
+
+export default about;
